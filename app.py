1dfdb37c08e9553062649c16f3d2134ff4ea8fd7
muatik <muatik@gmail.com>
2016-04-20 23:51:33 +0300

    adds user interfaces

--- app.py
+++ app.py
@@ -4,16 +4,33 @@ from dahi.context import Context
 from dahi.document import Document
 from dahi.documents import Documents
 from dahi.statement import Statement
-from flask import Flask, Blueprint, request, jsonify
+from flask import Flask, Blueprint, request, jsonify, send_from_directory
 from pymongo import MongoClient
 
 # TODO: a config file required for flask app
 api = Blueprint("api", __name__, url_prefix="/api/v1")
+ui = Blueprint("ui", __name__, static_folder="static/")
 
 # TODO: move the db initialization into a separate module
 db = MongoClient("mongodb://192.168.33.12")["dahi"]
 docs = Documents(db["docs"])
 botId = 12
+
+
+@ui.route('/index')
+def send_index():
+    return ui.send_static_file("index.html")
+
+
+@ui.route('/css/<path:path>')
+def send_css(path):
+    print path
+    return send_from_directory('static/css', path)
+
+
+@ui.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 
 @api.route("/docs/")
@@ -58,6 +75,7 @@ def getAnswer():
 def run():
     app = Flask(__name__)
     app.register_blueprint(api)
+    app.register_blueprint(ui)
     app.debug = True
     app.run()
 
